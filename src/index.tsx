19b9541ba54e91488be92d84e8bc4e8559166dfc
Romain BOIREAU <49552659+lokiodin@users.noreply.github.com>
2025-04-08 15:00:24 +0800

    t

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React, { StrictMode } from "react";
 import ReactDOM from 'react-dom/client';
-import { createRoot } from "react-dom/client";
 import "./styles.css";
 
 import App from "./App";
